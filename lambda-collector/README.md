902d1282fd7f836d65df30c7d10df22deef70d5a
alexjch <alex.jch@gmail.com>
2025-12-16 14:42:17 -0500

    Cleaning lambda handler code

--- lambda-collector/README.md
+++ lambda-collector/README.md
@@ -44,29 +44,23 @@ terraform apply
 
 Outputs:
 
-data_collector_endpoint = {
-  "authorization_type" = "NONE"
-  "cors" = tolist([])
-  "function_arn" = "arn:aws:lambda:us-west-2:713881820835:function:data-collector"
-  "function_name" = "data-collector"
-  "function_url" = "https://xiuiqeea2fxypgtvyr7mnozyuq0wxrwb.lambda-url.us-west-2.on.aws/"
-  "id" = "data-collector"
-  "invoke_mode" = "BUFFERED"
-  "qualifier" = ""
-  "region" = "us-west-2"
-  "timeouts" = null /* object */
-  "url_id" = "xiuiqeea2fxypgtvyr7mnozyuq0wxrwb"
-}
-
+data_collector_endpoint = "https://asjpvdj4wt4b4c4amde4btnwde0llqrh.lambda-url.us-west-2.on.aws/"
+lambda_function_arn = "arn:aws:lambda:us-west-2:713881820835:function:data-collector"
+s3_bucket_name = "storage-3595aa82-8cfc-4c41-a2f3-53a4c51e5bd7"
 ```
 
-**Note:** ```data_collector_endpoint.function_url``` this endpoint is neccesary for next steps
+**Note:** ```data_collector_endpoint``` this endpoint is neccesary for next steps
 
 ## Test collector with ```curl``` and ```aws s3```
 
 ```bash
-curl -vv -XPOST "https://xiuiqeea2fxypgtvyr7mnozyuq0wxrwb.lambda-url.us-west-2.on.aws/" -d '{"payload":"hello world!"}'
+# Send an object. Make a copy of the filename
+curl -X POST $(terraform output -raw data_collector_endpoint) -d '{"payload": "Hello World!!"}'
+{"message": "Data saved successfully", "filename": "20251216/1765915795-18341ebf-a294-407f-a8cd-506af4ace008"}
 
+# Check that is saved in s3, Using the filename printed in the HTTP response
+aws s3 cp s3://$(terraform output -raw s3_bucket_name)/20251216/1765915795-18341ebf-a294-407f-a8cd-506af4ace008 -
+{"payload": "Hello World!!"}
 ```
 
 ## TODO:
--- lambda-collector/function/lambda.py
+++ lambda-collector/function/lambda.py
@@ -1,6 +1,8 @@
 import os
 import json
+import base64
 import boto3
+import botocore
 import uuid
 import time
 import logging
@@ -10,49 +12,77 @@ logger.setLevel(logging.INFO)
 
 s3_client = boto3.client('s3')
 
+def _parse_json_body(event):
+    body = event.get("body", "")
+    if not body:
+        return None
 
-def save_message(bucket_name, key, message):
+    # If body is base64 encoded, decode first
+    if event.get("isBase64Encoded"):
+        body = base64.b64decode(body).decode("utf-8")
+
+    return json.loads(body)
+
+
+def _save_message(bucket_name, key, body):
     try:
         # Save to S3
         s3_client.put_object(
             Bucket=bucket_name,
             Key=key,
-            Body=json.dumps(message),
+            Body=json.dumps(body),
             ContentType='application/json'
         )
+    except botocore.exceptions.ClientError as e:
+            logger.exception(f"S3 client error while saving message {str(e)}")
+            return str(e)
     except Exception as e:
-        logger.error(f"Failed to upload receipt to S3: {str(e)}")
-        raise
+        logger.exception(f"Unexpected error while saving message {str(e)}")
+        return str(e)
+
+    return None
 
 
 def handler(event, context):
 
-    try:
-        # Get bucket name from environment variable
-        bucket_name = os.environ.get("S3_BUCKET", None)
-        if not bucket_name:
-            raise ValueError("Missing required environment variable S3_BUCKET")
-
-        # Generate a unique key with timestamp
-        timestamp = int(time.time())
-        date_dir = time.strftime('%Y%m%d', time.gmtime(timestamp))
-        key = f"{date_dir}/{timestamp}-{uuid.uuid4()}"
-    
-        save_message(bucket_name, key, event)
-
+    # Get bucket name from environment variable
+    bucket_name = os.environ.get("S3_BUCKET", None)
+    if not bucket_name:
+        logger.error("Missing required environment variable S3_BUCKET")
         return {
-            'statusCode': 200,
+            'statusCode': 400,
             'body': json.dumps({
-                'message': 'Data saved successfully',
-                'filename': key,
+                'message': 'S3_BUCKET Configuration error'
             })
         }
-    
-    except Exception as e:
+
+    # Generate a unique key with timestamp
+    timestamp = int(time.time())
+    date_dir = time.strftime('%Y%m%d', time.gmtime(timestamp))
+    key = f"{date_dir}/{timestamp}-{uuid.uuid4()}"
+
+    if (body := _parse_json_body(event)) is None:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'Failed to parse request body',
+                'error': 'Client request does not contain `body` key'
+            })
+        }
+
+    if (save_error_str := _save_message(bucket_name, key, body)):
         return {
             'statusCode': 500,
             'body': json.dumps({
-                'message': 'Error saving data',
-                'error': str(e)
+                'message': 'Failed to save message to S3',
+                'error': save_error_str,
             })
         }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Data saved successfully',
+            'filename': key,
+        })
+    }
