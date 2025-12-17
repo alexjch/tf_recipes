cfa529be85f3b8d93c8b51ac0be3c52bbc08a9a1
alexjch <alex.jch@gmail.com>
2025-12-17 01:26:43 -0500

    Use double quotes in handler
    Use double quotes in the lambda handler for consistency, before there
    were single and double quotes in different places.

--- lambda-collector/function/lambda.py
+++ lambda-collector/function/lambda.py
@@ -10,31 +10,31 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client("s3")
 
 def _get_secret(secret_name):
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(service_name='secretsmanager', region_name="us-west-2")
+    client = session.client(service_name="secretsmanager", region_name="us-west-2")
 
     try:
         # Retrieve the secret
         response = client.get_secret_value(SecretId=secret_name)
         # Decrypts secret using the associated KMS key.
-        return response['SecretString']
+        return response["SecretString"]
     except botocore.exceptions.ClientError as e:
-        logger.exception(f'Secrets manager returned an exception {str(e)}')
+        logger.exception(f"Secrets manager returned an exception {str(e)}")
 
     return None
 
 def _parse_json_body(event):
-    body = event.get('body', '')
+    body = event.get("body", "")
     if not body:
         return None
 
     # If body is base64 encoded, decode first
-    if event.get('isBase64Encoded'):
-        body = base64.b64decode(body).decode('utf-8')
+    if event.get("isBase64Encoded"):
+        body = base64.b64decode(body).decode("utf-8")
 
     return json.loads(body)
 
@@ -46,13 +46,13 @@ def _save_message(bucket_name, key, body):
             Bucket=bucket_name,
             Key=key,
             Body=json.dumps(body),
-            ContentType='application/json'
+            ContentType="application/json"
         )
     except botocore.exceptions.ClientError as e:
-            logger.exception(f'S3 client error while saving message {str(e)}')
+            logger.exception(f"S3 client error while saving message {str(e)}")
             return str(e)
     except Exception as e:
-        logger.exception(f'Unexpected error while saving message {str(e)}')
+        logger.exception(f"Unexpected error while saving message {str(e)}")
         return str(e)
 
     return None
@@ -61,95 +61,95 @@ def _save_message(bucket_name, key, body):
 def handler(event, context):
 
     # Get token from headers
-    authorization_header = event.get('headers', {}).get('authorization', '')
-    token = ''
-    if authorization_header.startswith('Bearer '):
-        _, token = authorization_header.split(' ')
+    authorization_header = event.get("headers", {}).get("authorization", "")
+    token = ""
+    if authorization_header.startswith("Bearer "):
+        _, token = authorization_header.split(" ")
 
     # Is token valid
     if not token:
         return {
-            'statusCode': 401,
-            'body': json.dumps({
-                'message': 'Unable to authorize',
-                'error': 'Authorization token is missing in request'
+            "statusCode": 401,
+            "body": json.dumps({
+                "message": "Unable to authorize",
+                "error": "Authorization token is missing in request"
             })
         }
 
     # Get secret name
-    secret_name = os.environ.get('SECRET_NAME', None)
+    secret_name = os.environ.get("SECRET_NAME", None)
     if not secret_name:
-        logger.error('Missing required environment variable SECRET_NAME')
+        logger.error("Missing required environment variable SECRET_NAME")
         return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'message': 'SECRET_NAME Configuration error',
-                'error': 'Secret manager is missconfigured'
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "SECRET_NAME Configuration error",
+                "error": "Secret manager is missconfigured"
             })
         }
 
     # Get secret
     secret = _get_secret(secret_name)
     if not secret:
-        logger.error(f'Unable to find secret: {secret_name}')
+        logger.error(f"Unable to find secret: {secret_name}")
         return {
-            'statusCode': 401,
-            'body': json.dumps({
-                'message': 'Unable to authorize',
-                'error': 'Failed loading secret manager'
+            "statusCode": 401,
+            "body": json.dumps({
+                "message": "Unable to authorize",
+                "error": "Failed loading secret manager"
             })
         }
 
     # Validate the token
     if token != secret:
-        logger.error(f'Invalid token: {token}')
+        logger.error(f"Invalid token: {token}")
         return {
-            'statusCode': 401,
-            'body': json.dumps({
-                'message': 'Unable to authorize',
-                'error': 'Authorization token is invalid'
+            "statusCode": 401,
+            "body": json.dumps({
+                "message": "Unable to authorize",
+                "error": "Authorization token is invalid"
             })
         }
 
     # Get bucket name from environment variable
-    bucket_name = os.environ.get('S3_BUCKET', None)
+    bucket_name = os.environ.get("S3_BUCKET", None)
     if not bucket_name:
-        logger.error('Missing required environment variable S3_BUCKET')
+        logger.error("Missing required environment variable S3_BUCKET")
         return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'message': 'S3_BUCKET Configuration error',
-                'error': 'Backend storage is missconfigured'
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "S3_BUCKET Configuration error",
+                "error": "Backend storage is missconfigured"
             })
         }
 
     # Generate a unique key with timestamp
     timestamp = int(time.time())
-    date_dir = time.strftime('%Y%m%d', time.gmtime(timestamp))
-    key = f'{date_dir}/{timestamp}-{uuid.uuid4()}'
+    date_dir = time.strftime("%Y%m%d", time.gmtime(timestamp))
+    key = f"{date_dir}/{timestamp}-{uuid.uuid4()}"
 
     if (body := _parse_json_body(event)) is None:
         return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'message': 'Failed to parse request body',
-                'error': 'Client request does not contain `body` key'
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Failed to parse request body",
+                "error": "Client request does not contain `body` key"
             })
         }
 
     if (save_error_str := _save_message(bucket_name, key, body)):
         return {
-            'statusCode': 500,
-            'body': json.dumps({
-                'message': 'Failed to save message to S3',
-                'error': save_error_str,
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "Failed to save message to S3",
+                "error": save_error_str,
             })
         }
 
     return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'message': 'Data saved successfully',
-            'filename': key,
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Data saved successfully",
+            "filename": key,
         })
     }
